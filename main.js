76b5b443988213e24fdda226880bdfc80a751b31
starrynskynight <ellayustiani@gmail.com>
2024-05-30 11:19:29 +0700

    Website Restoran

---
+++ main.js
@@ -0,0 +1,14 @@
+//SCROLL REVEAL ANIMATION
+
+// const sr = ScrollReveal({
+//     origin:'top',
+//     distance:'30px',
+//     duration: 2000,
+//     reset: true,
+// })
+
+// sr.reveal(' .visi, .misi', {
+//     interval: 200
+// })
+    
+
